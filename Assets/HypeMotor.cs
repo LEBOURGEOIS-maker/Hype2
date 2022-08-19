ae1c538853b9248a16325dbe478c2f425df54849
Fabibulle <fabduroule@live.fr>
2022-08-19 22:54:27 +0200

    park

--- Assets/HypeMotor.cs
+++ Assets/HypeMotor.cs
@@ -27,18 +27,20 @@ public class HypeMotor : MonoBehaviour {
     public string inputRight;
 
     public Vector3 jumpSpeed;
-    CapsuleCollider playerCollider;
+    CapsuleCollider HypeCollider;
+
+    // Le personnage est-il mort ?
+    public bool isDead = false;
 
     void Start () {
-        //_animator = GetComponent<Animator>();
         _animator = gameObject.GetComponent<Animator>();
-        playerCollider = gameObject.GetComponent<CapsuleCollider>();
+        HypeCollider = gameObject.GetComponent<CapsuleCollider>();
         rayHit = GameObject.Find("RayHit");
     }
 
     bool IsGrounded()
     {
-       return Physics.CheckCapsule(playerCollider.bounds.center, new Vector3(playerCollider.bounds.center.x, playerCollider.bounds.min.y - 0.1f, playerCollider.bounds.center.z), 0.08f, layerMask:9);
+       return Physics.CheckCapsule(HypeCollider.bounds.center, new Vector3(HypeCollider.bounds.center.x, HypeCollider.bounds.min.y - 0.1f, HypeCollider.bounds.center.z), 0.08f, layerMask:9);
     }
 
 	void Update () {
@@ -46,11 +48,7 @@ public class HypeMotor : MonoBehaviour {
         if (Input.GetKey(KeyCode.Z) && !Input.GetKey(KeyCode.LeftShift))
         {
             transform.Translate(0, 0, walkSpeed * Time.deltaTime);
-
-            //if (!isAttacking)
-            //{
-                _animator.Play("Walk");
-            //}
+            _animator.Play("Walk");
         }
                 
         // ## si on recule ## //
@@ -71,7 +69,7 @@ public class HypeMotor : MonoBehaviour {
         // Si on sprint
         if (Input.GetKey(KeyCode.Z) && Input.GetKey(KeyCode.LeftShift))
         {
-            transform.Translate(0, 0, (walkSpeed*1.5f) * Time.deltaTime);
+            transform.Translate(0, 0, runSpeed * Time.deltaTime);
             _animator.Play("Walk");
         }
         
