493e57b6c004f70004d7b256b58036d9a27c1a2c
LEBOURGEOIS <fabduroule@live.fr>
2025-06-29 23:03:43 +0200

    bridge in progress

--- Assets/Scripts/bridgeScript.cs
+++ Assets/Scripts/bridgeScript.cs
@@ -2,44 +2,22 @@ using UnityEngine;
 
 public class bridgeScript : MonoBehaviour
 {
-    public Animator bridgeAnimator; // À assigner dans l'inspecteur
-    private bool playerInsideZone = false;
-    private bool isOpen = false;
+    public Animator bridgeAnimator;
 
-    void Update()
+    void Start()
     {
-        if (playerInsideZone && Input.GetKeyDown(KeyCode.Space))
-        {
-            if (!isOpen)
-            {
-                bridgeAnimator.SetTrigger("opening");
-                isOpen = true;
-                Debug.Log("Espace appuyé : ouverture du pont.");
-            }
-            else
-            {
-                bridgeAnimator.SetTrigger("closing");
-                isOpen = false;
-                Debug.Log("Espace appuyé : fermeture du pont.");
-            }
-        }
+        bridgeAnimator = GetComponent<Animator>();
     }
 
     void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player"))
-        {
-            playerInsideZone = true;
-            Debug.Log("Le joueur est entré dans la zone du pont.");
-        }
+        bridgeAnimator.SetTrigger("opening");
+        Debug.Log("Le joueur est entré dans la zone du pont.");
     }
 
     void OnTriggerExit(Collider other)
     {
-        if (other.CompareTag("Player"))
-        {
-            playerInsideZone = false;
-            Debug.Log("Le joueur a quitté la zone du pont.");
-        }
+        bridgeAnimator.SetTrigger("closing");
+        Debug.Log("Le joueur a quitté la zone du pont.");
     }
 }
