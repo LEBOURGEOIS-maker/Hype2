ba2a0db2f416a6dbcbd249304eba9739bdac0e65
LEBOURGEOIS <fabduroule@live.fr>
2025-06-30 14:34:10 +0200

    bridge works

--- Assets/Scripts/Managers/bridgeScript.cs
+++ Assets/Scripts/Managers/bridgeScript.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class bridgeScript : MonoBehaviour
 {
     public Animator bridgeAnimator;
+    private bool isOpen = false;
 
     void Start()
     {
@@ -11,13 +12,22 @@ public class bridgeScript : MonoBehaviour
 
     void OnTriggerEnter(Collider other)
     {
-        bridgeAnimator.SetTrigger("opening");
-        Debug.Log("Le joueur est entré dans la zone du pont.");
+        Debug.Log("OnTriggerEnter function");
+        if (!isOpen)
+        {
+            isOpen = true;
+            bridgeAnimator.SetTrigger("opening");
+            Debug.Log("Le joueur est entré dans la zone du pont.");
+        }
     }
-
     void OnTriggerExit(Collider other)
     {
-        bridgeAnimator.SetTrigger("closing");
-        Debug.Log("Le joueur a quitté la zone du pont.");
+        Debug.Log("OnTriggerExit function");
+        if (isOpen)
+        {
+            isOpen = false;
+            bridgeAnimator.SetTrigger("closing");
+            Debug.Log("Le joueur a quitté la zone du pont.");
+        }
     }
 }
